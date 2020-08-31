894e957513f2dc8efd20abe7346d6bedde1acdc7
miyamotokeisuke <kanabn000@gmail.com>
2020-08-31 21:58:23 +0900

    miya

--- Assets/script/ImageCangescript/ImageChange5.cs
+++ Assets/script/ImageCangescript/ImageChange5.cs
@@ -6,29 +6,51 @@ using UnityEngine.SceneManagement;
 
 public class ImageChange5 : MonoBehaviour
 {
+	float fadeSpeed = 0.005f;     
+    float red, green, blue, alfa;
+    public bool isFadeOut = false;
+
+    Image fadeImage;
+
     // Start is called before the first frame update
     void Start()
     {
+        fadeImage = GetComponent<Image>();
+        red = fadeImage.color.r;
+        green = fadeImage.color.g;
+        blue = fadeImage.color.b;
+        alfa = fadeImage.color.a;
+    }
+
+    void Update()
+    {
+        if (isFadeOut)
+        {
+            StartFadeOut();
+        }
+    }
+
+    void StartFadeOut()
+    {
+        fadeImage.enabled = true;  // a)パネルの表示をオンにする
+        alfa += fadeSpeed;         // b)不透明度を徐々にあげる
+        SetAlpha();               // c)変更した透明度をパネルに反映する
+        if (alfa >= 1)
+        {             // d)完全に不透明になったら処理を抜ける
+            isFadeOut = false;
+        }
         StartCoroutine("ChangeImage6");
+    }
+
+    void SetAlpha()
+    {
+        fadeImage.color = new Color(red, green, blue, alfa);
     }
 
     IEnumerator ChangeImage6()
     {
-        // オブジェクトの取得
-        GameObject image_object = GameObject.Find("Image6");
-        Image image_component = image_object.GetComponent<Image>();
-        // リソースから、切り替える画像を取得
-        Texture2D clear6 = Resources.Load("GameClear Image") as Texture2D;
-
-        //3秒停止
-        yield return new WaitForSeconds(2f);
-
-        //反映させる
-        image_component.sprite = Sprite.Create(clear6, new Rect(0, 0, clear6.width, clear6.height), Vector2.zero);
-
-
-        //3秒停止
-        yield return new WaitForSeconds(1f);
+        //秒停止
+        yield return new WaitForSeconds(3f);
 
         SceneManager.LoadScene("GameClear");
     }
