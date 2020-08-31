a3e1bff7be0156d865bb7d7fad97de08c5c15bf6
miyamotokeisuke <kanabn000@gmail.com>
2020-08-31 16:09:45 +0900

    miya

--- Assets/script/ImageCangescript/ImageChange5.cs
+++ Assets/script/ImageCangescript/ImageChange5.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class ImageChange5 : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -16,12 +18,18 @@ public class ImageChange5 : MonoBehaviour
         GameObject image_object = GameObject.Find("Image6");
         Image image_component = image_object.GetComponent<Image>();
         // リソースから、切り替える画像を取得
-        Texture2D clear6 = Resources.Load("clear6") as Texture2D;
+        Texture2D clear6 = Resources.Load("GameClear Image") as Texture2D;
 
         //3秒停止
         yield return new WaitForSeconds(2f);
 
         //反映させる
         image_component.sprite = Sprite.Create(clear6, new Rect(0, 0, clear6.width, clear6.height), Vector2.zero);
+
+
+        //3秒停止
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene("GameClear");
     }
 }
