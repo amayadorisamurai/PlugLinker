63f3f804bd7d321b1f1d3e001deddfe38412774f
miyamotokeisuke <kanabn000@gmail.com>
2020-11-09 02:28:48 +0900

    miya

--- Assets/script/EndSeni.cs
+++ Assets/script/EndSeni.cs
@@ -11,12 +11,10 @@ public class EndSeni : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
