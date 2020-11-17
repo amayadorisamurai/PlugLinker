c3edc31e9df3f1c3cb0652f8e91d18ffe6429e24
miyamotokeisuke <kanabn000@gmail.com>
2020-11-17 14:27:53 +0900

    miya

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -4,52 +4,35 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public Vector2 Speed = new Vector2(0.05f, 0.05f);
 
     // Update is called once per frame
-    private void Update()
-    {
+    private void Update(){
         //移動
         Move();
     }
 
     //移動関数
-    void Move()
-    {
+    void Move(){
 
         // 現在位置をPositionに代入
-
         Vector2 Position = transform.position;
 
         // 左キーを押し続けていたら
-
-        if (Input.GetKey("left"))
-        {
+        if (Input.GetKey(KeyCode.LeftArrow)){
             // 代入したPositionに対して加算減算を行う
             Position.x -= Speed.x;
-
         }
-        else if (Input.GetKey("right"))  //右キーを押し続けていたら
-        { 
+        if (Input.GetKey(KeyCode.RightArrow)){ 
             Position.x += Speed.x;
         }
-        else if (Input.GetKey("up"))  //上キーを押し続けていたら
-        { 
+        if (Input.GetKey(KeyCode.UpArrow)){ 
             Position.y += Speed.y;
         }
-        else if (Input.GetKey("down"))  //下キーを押し続けていたら
-        { 
+        if (Input.GetKey(KeyCode.DownArrow)){ 
             Position.y -= Speed.y;
         }
-
         // 現在の位置にPositionを代入する
         transform.position = Position;
-
     }
 }
