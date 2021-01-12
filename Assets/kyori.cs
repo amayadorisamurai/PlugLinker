cb1eafa96a45f416b0264804b01d14ff4194d1b7
miyamotokeisuke <kanabn000@gmail.com>
2021-01-12 14:17:02 +0900

    miya

---
+++ Assets/kyori.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class kyori : MonoBehaviour
+{
+    int score;
+    GameObject MetleText;
+
+    public void AddScore()
+    {
+        this.score = 100;
+    }
+
+    void Start()
+    {
+        this.MetleText = GameObject.Find("Score");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos1 = new Vector2(10.0f, 24.0f);//ゴール地点を入れる（新しく変数を作って入れる）
+        Vector2 pos2 = new Vector2(3.0f, 10.0f);//現在地を入れる（）
+        Debug.Log(Vector2.Distance(pos1, pos2));
+        Debug.Log((pos1 - pos2).magnitude);
+        Debug.Log(Mathf.Sqrt(Mathf.Pow(pos1.x - pos2.x, 2) + Mathf.Pow(pos1.y - pos2.y, 2)));
+    }
+}
---
+++ Assets/script/sinkoudo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sinkoudo : MonoBehaviour
+{
+    Slider _slider;
+    void Start()
+    {
+        // スライダーを取得する
+        _slider = GameObject.Find("Slider").GetComponent<Slider>();
+    }
+
+    float _hp = 0;
+    void Update()
+    {
+        _hp += 0.01f;
+        if (_hp > 1)
+        { 
+            _hp = 0;
+        }
+
+        _slider.value = _hp;
+    }
+}
